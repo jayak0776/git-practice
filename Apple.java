aa8bc2d7d0a08a7fcaca7ba580e4334559348372
Gubbala Jayakumar <115056837+jayak0776@users.noreply.github.com>
2025-08-26 14:46:14 +0530

    Apple

--- Apple.java
+++ Apple.java
@@ -1,4 +1,8 @@
 public class Apple{
+
+    public void getMessage(){
+        System.out.println("I'm Apple!");
+    }
     public static void main(String [] args){
         System.out.println("Hello World!");
         System.out.println("Hello Everyone!");
